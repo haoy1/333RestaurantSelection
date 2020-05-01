1ee4144088879c058b0f1ca581ab6c0e9c6e4391
haoy1 <haoy1@rose-hulman.edu>
2020-05-01 04:21:12 -0400

    Login

---
+++ 333RestaurantSelectionProject/src/HorizontalLayout.java
@@ -0,0 +1,24 @@
+import java.awt.Button;
+
+public class HorizontalLayout {
+
+	public HorizontalLayout(Button log, Button register) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setMargin(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSpacing(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMaxWidth(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ 333RestaurantSelectionProject/src/LoginOverlay.java
@@ -0,0 +1,29 @@
+
+public class LoginOverlay {
+
+	public void setAction(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setOpened(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTitle(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDescription(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
