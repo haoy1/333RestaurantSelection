78587122d8d91228b3d03de740fa902331cec46b
haoy1 <haoy1@rose-hulman.edu>
2020-05-03 15:10:13 -0400

    .

---
+++ 333RestaurantSelectionProject/src/DatabaseConnection.java
@@ -0,0 +1,55 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+	//DO NOT EDIT THIS STRING, YOU WILL RECEIVE NO CREDIT FOR THIS TASK IF THIS STRING IS EDITED
+	private final String SampleURL = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}}";
+
+	private Connection connection = null;
+
+	private String databaseName;
+	private String serverName;
+
+	public DatabaseConnection(String serverName, String databaseName) {
+		//DO NOT CHANGE THIS METHOD
+		this.serverName = serverName;
+		this.databaseName = databaseName;
+	}
+
+	public boolean connect(String user, String pass) {
+		// Task 1
+		//BUILD YOUR CONNECTION STRING HERE USING THE SAMPLE URL ABOVE
+		String URL = "jdbc:sqlserver://"+this.serverName+";databaseName="+this.databaseName+";user="+user+";password={"+pass+"}";
+		try {
+			connection = DriverManager.getConnection(URL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if(connection != null) {
+			return true;
+		}
+		return false;
+	}
+	
+
+	public Connection getConnection() {
+		return this.connection;
+	}
+	
+	public void closeConnection() {
+		try {
+			this.connection.close();
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	public String getDatabaseName() {
+		//  Auto-generated method stub
+		return this.databaseName;
+	}
+
+}
--- 333RestaurantSelectionProject/src/Login.js
+++ 333RestaurantSelectionProject/src/Login.js
@@ -2,6 +2,7 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+const connectedMsg = document.getElementById("connection-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -12,6 +13,28 @@ loginButton.addEventListener("click", (e) => {
         alert("You have successfully logged in.");
         location.reload();
     } else {
-        loginErrorMsg.style.opacity = 1;
+    	loginErrorMsg.style.opacity = 1;
     }
 })
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "golem.csse.rose-hulman.edu",
+  user: "haoy1@rose-hulman.edu",
+  password: "Horryno1",
+  database: "RestaurantSelection"
+});
+
+con.connect(function(err) { 
+  if (err) {
+	  console.error(err);
+	  return;
+  }
+  console.log("Connected!");
+  connectedMsg.style.opacity = 1;
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+});
