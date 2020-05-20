09a523cbb3e18c5cf44815a0d15d788a0170b78f
chenxiany11 <chenx11@rose-human.edu>
2020-05-20 00:06:25 -0400

    finish data import

--- 333RestaurantSelectionProject/src/ImportData.java
+++ 333RestaurantSelectionProject/src/ImportData.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
 import org.apache.poi.EncryptedDocumentException;
@@ -27,18 +26,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ImportData {
 
 	static DatabaseConnection dbService;
-	
+	static Connection c;
 	public static void main(String[] args) throws IOException, EncryptedDocumentException, NumberFormatException, SQLException {
-		DatabaseConnection dbService;
+		
 		System.out.print("Import Excel Data ------>\n");
 		dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
 		dbService.connect("haoy1", "Horryno1");
+		c = dbService.getConnection();
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			System.out.print("Import from Excel\n" + "1: Insert MenuItem\n" + "2: Insert Restaurant\n"
-					+ "3: Insert users\n" + "4: Insert FoodRate\n" + "5: Insert Order Item\n" 
-					+ "6: Clear all tables\n"+"7: Exit\n");
+					+ "3: Insert users\n" + "4: Insert FoodRate\n" + "5: Insert Order Item\n"  + "6: Insert Restaurant rate\n" 
+					+"7: Insert Administrator\n" + "8: Clear all tables\n"+"9: Exit\n");
 			String type = scanner.nextLine();
 			switch (type) {
 			case "1":
@@ -58,9 +58,15 @@ public class ImportData {
 				insertOrderItem();
 				break;
 			case "6":
-				ClearAllTable();
+				insertRestaurantRate();
 				break;
 			case "7":
+				insertAdministrator();
+				break;
+			case "8":
+				ClearAllTable();
+				break;
+			case "9":
 				System.out.println("Exit!");
 				dbService.closeConnection();
 				System.exit(0);
@@ -71,61 +77,106 @@ public class ImportData {
 	}
 		
 
-	private static void insertOrderItem() {
-//		System.out.println("Enter Excel data: ");
-//		Scanner input = new Scanner(System.in);
-//		String excelName = input.nextLine();
-//
-//		FileInputStream fis = new FileInputStream(excelName);
-//		XSSFWorkbook wb = (XSSFWorkbook) WorkbookFactory.create(fis);
-//		XSSFSheet sh = wb.getSheet("Sheet1");
-//		ArrayList<Object> list = new ArrayList<>();
-//		ImportDataService ds = new ImportDataService(dbService);
-//
-//		// Iterate through each rows one by one
-//		Iterator<Row> rowIterator = sh.iterator();
-//		while (rowIterator.hasNext()) {
-//			Row row = rowIterator.next();
-//			if (row.getRowNum() == 0) {
-//				continue; // just skip the rows if row number is 0 or 1
-//			} else {
-//				Iterator<Cell> cellIterator = row.cellIterator();
-//				while (cellIterator.hasNext()) {
-//					Cell cell = cellIterator.next();
-//					list.add(cell.toString());
-//				}
-//				ds.InsertOrderItem(list.get(0), list.get(1), (int) Double.parseDouble((list.get(2))));
-//				list = new ArrayList<>();
-//			}
-//		}
-//		System.out.println("Import Complete");
-	}
-
-
-	private static void insertFoodRate() throws IOException, SQLException {
-		String excelFilePath = "foodrate.xlsx";
+	private static void insertAdministrator()throws IOException, SQLException {
+		String excelFilePath = "ad.xlsx";
 		FileInputStream inputStream = new FileInputStream(excelFilePath);
 		@SuppressWarnings("resource")
 		Workbook workbook = new XSSFWorkbook(inputStream);
 		Sheet firstSheet = workbook.getSheet("Sheet1");
-		ImportDataService is = new ImportDataService(dbService);
+		ImportDataService is = new ImportDataService(c);
 		ArrayList<String> list = new ArrayList<>();
 		Iterator<Row> rowIterator = firstSheet.iterator();
 		
 		while (rowIterator.hasNext()) {
 			Row row = rowIterator.next();
-			if (row.getRowNum() == 0) {
-				continue; // just skip the rows if row number is 0 or 1
-			} else {
+			
 				Iterator<Cell> cellIterator = row.cellIterator();
 				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
 					list.add(cell.toString());
 				}
-				is.InsertFoodRate(list.get(0),list.get(1), list.get(2),list.get(3));
+				System.out.println("ready to import");
+				is.InsertAd(list.get(0),list.get(1));
+				list = new ArrayList<>();
+		}
+		System.out.println("Import Complete");
+	}
+
+
+	private static void insertRestaurantRate() throws IOException, SQLException{
+		String excelFilePath = "resRate.xlsx";
+		FileInputStream inputStream = new FileInputStream(excelFilePath);
+		@SuppressWarnings("resource")
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet firstSheet = workbook.getSheet("Sheet1");
+		ImportDataService is = new ImportDataService(c);
+		ArrayList<String> list = new ArrayList<>();
+		Iterator<Row> rowIterator = firstSheet.iterator();
+		
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					list.add(cell.toString());
+				}
+				
+				is.InsertResRate(list.get(0),list.get(1), list.get(2),list.get(3));
 				list = new ArrayList<>();
 			}
-		}
+		System.out.println("Import Complete");
+	}
+
+
+	private static void insertOrderItem() throws IOException, SQLException{
+		String excelFilePath = "order.xlsx";
+		FileInputStream inputStream = new FileInputStream(excelFilePath);
+		@SuppressWarnings("resource")
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet firstSheet = workbook.getSheet("Sheet1");
+		ImportDataService is = new ImportDataService(c);
+		ArrayList<String> list = new ArrayList<>();
+		Iterator<Row> rowIterator = firstSheet.iterator();
+		
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					list.add(cell.toString());
+				}
+				
+				is.InsertOrder(list.get(0),list.get(1), list.get(2),list.get(3),list.get(4));
+				list = new ArrayList<>();
+			}
+		System.out.println("Import Complete");
+	}
+
+
+	private static void insertFoodRate() throws IOException, SQLException {
+		String excelFilePath = "foodRate.xlsx";
+		FileInputStream inputStream = new FileInputStream(excelFilePath);
+		@SuppressWarnings("resource")
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		Sheet firstSheet = workbook.getSheet("Sheet1");
+		ImportDataService is = new ImportDataService(c);
+		ArrayList<String> list = new ArrayList<>();
+		Iterator<Row> rowIterator = firstSheet.iterator();
+		
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					list.add(cell.toString());
+				}
+				
+				is.InsertFoodRate(list.get(0),list.get(1), list.get(2),list.get(3),list.get(4));
+				list = new ArrayList<>();
+			}
 		System.out.println("Import Complete");
 	}
 
@@ -137,23 +188,21 @@ public class ImportData {
 		@SuppressWarnings("resource")
 		Workbook workbook = new XSSFWorkbook(inputStream);
 		Sheet firstSheet = workbook.getSheet("Sheet1");
-		ImportDataService is = new ImportDataService(dbService);
+		ImportDataService is = new ImportDataService(c);
 		ArrayList<String> list = new ArrayList<>();
 		Iterator<Row> rowIterator = firstSheet.iterator();
 		
 		while (rowIterator.hasNext()) {
 			Row row = rowIterator.next();
-			if (row.getRowNum() == 0) {
-				continue; // just skip the rows if row number is 0 or 1
-			} else {
+			
 				Iterator<Cell> cellIterator = row.cellIterator();
 				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
 					list.add(cell.toString());
 				}
+				System.out.println("ready to import");
 				is.InsertUser(list.get(0),list.get(1));
 				list = new ArrayList<>();
-			}
 		}
 		System.out.println("Import Complete");
 	}
@@ -161,7 +210,7 @@ public class ImportData {
 
 	private static void ClearAllTable()
 			throws EncryptedDocumentException, IOException, NumberFormatException, SQLException {
-		ImportDataService ds = new ImportDataService(dbService);
+		ImportDataService ds = new ImportDataService(c);
 		ds.ClearALL();
 
 
@@ -175,15 +224,13 @@ public class ImportData {
 		@SuppressWarnings("resource")
 		Workbook workbook = new XSSFWorkbook(inputStream);
 		Sheet firstSheet = workbook.getSheet("Sheet1");
-		ImportDataService is = new ImportDataService(dbService);
+		ImportDataService is = new ImportDataService(c);
 		ArrayList<String> list = new ArrayList<>();
 		Iterator<Row> rowIterator = firstSheet.iterator();
 		
 		while (rowIterator.hasNext()) {
 			Row row = rowIterator.next();
-			if (row.getRowNum() == 0) {
-				continue; // just skip the rows if row number is 0 or 1
-			} else {
+			
 				Iterator<Cell> cellIterator = row.cellIterator();
 				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
@@ -192,7 +239,6 @@ public class ImportData {
 				is.InsertMenu(list.get(0),list.get(1), list.get(2));
 				list = new ArrayList<>();
 			}
-		}
 		System.out.println("Import Complete");
 	}
 
@@ -203,7 +249,7 @@ public class ImportData {
 		@SuppressWarnings("resource")
 		Workbook workbook = new XSSFWorkbook(inputStream);
 		Sheet firstSheet = workbook.getSheetAt(0);
-		ImportDataService is = new ImportDataService(dbService);
+		ImportDataService is = new ImportDataService(c);
 		ArrayList<String> list = new ArrayList<>();
 		Iterator<Row> rowIterator = firstSheet.iterator();
 		
--- 333RestaurantSelectionProject/src/ImportDataService.java
+++ 333RestaurantSelectionProject/src/ImportDataService.java
@@ -1,5 +1,9 @@
 import java.io.FileInputStream;
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -8,6 +12,10 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import java.util.Base64;
+import java.util.Random;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.swing.JOptionPane;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -18,19 +26,20 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ImportDataService {
 
-	private DatabaseConnection dbService = null;
+	 DatabaseConnection dbService = null;
+	 static final Random RANDOM = new SecureRandom();
+	 static final Base64.Encoder enc = Base64.getEncoder();
+	 Connection c;
 
-	public ImportDataService() {
-	}
 
-	public ImportDataService(DatabaseConnection dbService) {
-		this.dbService = dbService;
+	public ImportDataService(Connection con) {
+		c = con;
 	}
 
 	public boolean InsertMenu(String name, String price, String id) throws SQLException {
-		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
-     	dbService.connect("haoy1", "Horryno1");
-     	Connection c = this.dbService.getConnection();
+//		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
+//     	dbService.connect("haoy1", "Horryno1");
+  
  		CallableStatement stm = null;
 		try {
 			stm = c.prepareCall("{?= call [chex11].[import_Menu](?,?,?)}");
@@ -82,9 +91,9 @@ public class ImportDataService {
 	}
 
 	public void InsertRestaurant(String name, String addr, String string, String cat, String opent, String closet) {
-		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
-     	dbService.connect("haoy1", "Horryno1");
-     	Connection c = this.dbService.getConnection();
+//		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
+//     	dbService.connect("haoy1", "Horryno1");
+     	//Connection c = this.dbService.getConnection();
  		CallableStatement stm = null;
 		try {
 			stm = c.prepareCall("{?= call [chex11].[addResTest](?,?,?,?,?,?)}");
@@ -113,22 +122,22 @@ public class ImportDataService {
 
 	}
 
-	public void InsertFoodRate(String star, String review, String res, String user) {
+	public void InsertFoodRate(String user, String star, String review, String item, String res) {
 		// Auto-generated method stub
-		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
-     	dbService.connect("haoy1", "Horryno1");
-     	Connection c = this.dbService.getConnection();
+//		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
+//     	dbService.connect("haoy1", "Horryno1");
+     	//Connection c = this.dbService.getConnection();
  		CallableStatement stm = null;
 		try {
-			stm = c.prepareCall("{ call [chex11].[import_food_rate](?,?,?,?)}");
+			stm = c.prepareCall("{call [chex11].[import_food_rate](?,?,?,?,?)}");
 			//stm.registerOutParameter(1, Types.INTEGER);
-			stm.setString(1, star);
-			stm.setString(2, review);
-			stm.setString(3, res);
-			stm.setString(4, user);
+			stm.setString(1, user);
+			stm.setFloat(2, (int) Double.parseDouble(star));
+			stm.setString(3, review);
+			stm.setString(4, item);
+			stm.setString(5, res);
 			stm.execute();
 			System.out.println("Import xx Complete");
-			
 			return;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -138,9 +147,123 @@ public class ImportDataService {
 	}
 
 	public void InsertUser(String name, String pass) {
-		Register_Form rf = new Register_Form(name, pass);
-		rf.addUser(name, pass);
-		
+//		this.dbService = new DatabaseConnection("golem.csse.rose-hulman.edu", "RestaurantSelection");
+//     	dbService.connect("haoy1", "Horryno1");
+		//Connection c = this.dbService.getConnection();
+		CallableStatement stm = null;
+		byte[] salt = getNewSalt();
+		try {
+			stm = c.prepareCall("{?=call dbo.addUser(?,?,?)}");
+			stm.registerOutParameter(1, Types.INTEGER);
+			stm.setString(2, name);
+			stm.setString(3, hashPassword(salt, pass));
+			stm.setBytes(4, salt);
+			stm.execute();
+			System.out.println("Import xx Complete");
+			if(stm.getInt(1) == 1) 
+				System.out.println( "Username already existed");
+			else if(name.isEmpty() || pass.isEmpty())
+				System.out.println( "Username and Password can't be empty");
+			else
+				System.out.println("Successfully Registered");
+			return;
+		} catch (SQLException e) {
+			System.out.println("Failed ");
+			e.printStackTrace();
+		}
+		return;
 	}
+
+	 public byte[] getNewSalt() {
+			byte[] salt = new byte[16];
+			RANDOM.nextBytes(salt);
+			return salt;
+		}
+	    
+	    public String hashPassword(byte[] salt, String password) {
+
+			KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+			SecretKeyFactory f;
+			byte[] hash = null;
+			try {
+				f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+				hash = f.generateSecret(spec).getEncoded();
+			} catch (NoSuchAlgorithmException e) {
+				JOptionPane.showMessageDialog(null, "An error occurred during password hashing. See stack trace.");
+				e.printStackTrace();
+			} catch (InvalidKeySpecException e) {
+				JOptionPane.showMessageDialog(null, "An error occurred during password hashing. See stack trace.");
+				e.printStackTrace();
+			}
+			return getStringFromBytes(hash);
+		}
+	    public String getStringFromBytes(byte[] data) {
+			return enc.encodeToString(data);
+		}
+
+		public void InsertOrder(String item, String res, String user, String time, String qty) {
+			CallableStatement stm = null;
+			try {
+				stm = c.prepareCall("{call chex11.insert_order(?,?,?,?,?)}");
+				stm.setString(1, item);
+				stm.setString(2, res);
+				stm.setString(3, user);
+				stm.setString(4, time);
+				stm.setInt(5, (int) Double.parseDouble(qty));
+				stm.execute();
+				System.out.println("Import xx Complete");
+				
+				return;
+			} catch (SQLException e) {
+				System.out.println("Failed");
+				e.printStackTrace();
+			}
+			return;
+		}
+
+		public void InsertResRate(String user, String res, String star, String review) {
+			CallableStatement stm = null;
+			try {
+				stm = c.prepareCall("{call [chex11].[insert_RestaurantRate](?,?,?,?)}");
+				//stm.registerOutParameter(1, Types.INTEGER);
+				stm.setString(1, user);
+				stm.setString(2, res);
+				stm.setInt(3, (int) Double.parseDouble(star));
+				stm.setString(4, review);
+
+				stm.execute();
+				System.out.println("Import xx Complete");
+				return;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return;
+
+		}
+
+		public void InsertAd(String name, String pass) {
+			CallableStatement stm = null;
+			byte[] salt = getNewSalt();
+			try {
+				stm = c.prepareCall("{?=call [dbo].[CreateAdministrator](?,?,?)}");
+				stm.registerOutParameter(1, Types.INTEGER);
+				stm.setString(2, name);
+				stm.setString(3, hashPassword(salt, pass));
+				stm.setBytes(4, salt);
+				stm.execute();
+				System.out.println("Import xx Complete");
+				if(stm.getInt(1) == 1) 
+					System.out.println( "Username already existed");
+				else if(name.isEmpty() || pass.isEmpty())
+					System.out.println( "Username and Password can't be empty");
+				else
+					System.out.println("Successfully Registered");
+				return;
+			} catch (SQLException e) {
+				System.out.println("Failed ");
+				e.printStackTrace();
+			}
+			return;
+		}
 
 }
